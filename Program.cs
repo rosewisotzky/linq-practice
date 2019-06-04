db438c7fe063a80b9d67baff704fefac73ea253a
Rose Wisotzky <rose@rosewisotzky.com>
2019-06-04 10:59:43 -0500

    fruits with L

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace linq
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+            string L = "L";
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                where fruit.StartsWith(L)
+                orderby fruit ascending
+                select fruit;
+
+            foreach (string fruit in LFruits)
+            {
+                Console.WriteLine($"{fruit}");
+
+            }
         }
     }
 }
